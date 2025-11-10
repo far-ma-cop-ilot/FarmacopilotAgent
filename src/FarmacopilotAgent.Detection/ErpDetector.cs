069d59a46041a90bfac27bd8e182867340fab452
far-ma-cop-ilot <farmaciabueno@farmacopilot.com>
2025-11-10 17:55:25 +0100

    Create ErpDetector.cs

---
+++ src/FarmacopilotAgent.Detection/ErpDetector.cs
@@ -0,0 +1,10 @@
+public class ErpDetector : IErpDetector
+{
+    public ErpInfo DetectErp()
+    {
+        // 1. Buscar claves registro HKLM
+        // 2. Detectar archivos configuración
+        // 3. Identificar versión
+        // 4. Determinar tipo BD
+    }
+}
