adf31e6463a3bfb3e2b98da37d02189d03d4e8c0
far-ma-cop-ilot <farmaciabueno@farmacopilot.com>
2025-11-10 17:54:04 +0100

    Create DPAPIHelper.cs

---
+++ src/FarmacopilotAgent.Core/Security/DPAPIHelper.cs
@@ -0,0 +1,6 @@
+public static class DPAPIHelper
+{
+    public static string Encrypt(string plainText) { /* ... */ }
+    public static string Decrypt(string encryptedText) { /* ... */ }
+    public static bool IsEncrypted(string text) { /* ... */ }
+}
