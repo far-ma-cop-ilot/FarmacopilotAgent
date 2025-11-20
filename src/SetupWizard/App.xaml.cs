80de3659e142a3c567eca831b7cec178797082c7
far-ma-cop-ilot <farmaciabueno@farmacopilot.com>
2025-11-20 18:41:33 +0100

    Create App.xaml.cs

---
+++ src/SetupWizard/App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace SetupWizard
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Inicialización global si necesitas (e.g., logging)
+        }
+    }
+}
